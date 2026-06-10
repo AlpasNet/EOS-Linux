dbabc52e1159caec802adb344420f385ee702111
AlpasNet <117809035+AlpasNet@users.noreply.github.com>
2026-06-10 15:09:38 +0200

    Add eos-linux-install.sh for user replacement
    Script replaces occurrences of the old username with a new username in files within the home directory.

---
+++ eos-linux-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OLD_USER="seijeeos"
+NEW_USER="$1"
+TARGET_DIR="home"
+
+if [ -z "$NEW_USER" ]; then
+    echo "Error : you must put your username."
+    echo "Usage : $0 username"
+    exit 1
+fi
+
+echo "Replacing '$OLD_USER' by '$NEW_USER' ..."
+
+grep -RIl "$OLD_USER" "home" | while read -r file; do
+    sed -i "s|$OLD_USER|$NEW_USER|g" "$file"
+done
