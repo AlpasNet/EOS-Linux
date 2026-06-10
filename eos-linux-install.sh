d2e9874a4b7ee37b5d1ce9394668e2bda779e588
AlpasNet <117809035+AlpasNet@users.noreply.github.com>
2026-06-10 15:18:40 +0200

    Replace OnlyOffice download with OpenShot download

--- eos-linux-install.sh
+++ eos-linux-install.sh
@@ -23,5 +23,5 @@ done
 # DOWNLOAD SOFTWARES
 ###############################################################
 wget -O ./home/Programs/GIMP.AppImage "https://download.gimp.org/gimp/v3.2/linux/GIMP-3.2.4-x86_64.AppImage"
-wget -O ./home/Programs/OnlyOffice.AppImage "https://github.com/ONLYOFFICE/appimage-desktopeditors/releases/download/v9.4.0/DesktopEditors-x86_64.AppImage"
+wget -O ./home/Programs/OpenShot.AppImage "https://github.com/OpenShot/openshot-qt/releases/download/v3.5.1/OpenShot-v3.5.1-x86_64.AppImagee"
 wget -O ./home/Programs/upscayl.AppImage "https://github.com/upscayl/upscayl/releases/download/v2.15.0/upscayl-2.15.0-linux.AppImage"
