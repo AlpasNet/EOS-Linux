f64bf28a5384f5554c5901bbd59eda6292bd7ba7
AlpasNet <117809035+AlpasNet@users.noreply.github.com>
2026-06-10 18:50:21 +0200

    Refactor software download and user file copy
    Removed redundant software download commands and added a command to copy files to the new user directory.

--- eos-linux-install.sh
+++ eos-linux-install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 ###############################################################
+# DOWNLOAD SOFTWARES
+###############################################################
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt remove elise haruna kate kmahjongg kmines kpat ksudoku libreoffice-* neochat
+sudo snap remove thunderbird
+sudo apt autoremove
+
+sudo apt install ntfs-3g vlc filezilla spotify
+sudo snap install onlyoffice-desktopeditors
+
+#FOR NVIDIA USERS
+#sudo apt-get install nvidia-dkms-595
+
+wget -O ./home/Programs/GIMP.AppImage "https://download.gimp.org/gimp/v3.2/linux/GIMP-3.2.4-x86_64.AppImage"
+wget -O ./home/Programs/OpenShot.AppImage "https://github.com/OpenShot/openshot-qt/releases/download/v3.5.1/OpenShot-v3.5.1-x86_64.AppImagee"
+wget -O ./home/Programs/upscayl.AppImage "https://github.com/upscayl/upscayl/releases/download/v2.15.0/upscayl-2.15.0-linux.AppImage"
+
+###############################################################
 # CHANGE USERNAME
 ###############################################################
 
@@ -19,9 +40,4 @@ grep -RIl "$OLD_USER" "home" | while read -r file; do
     sed -i "s|$OLD_USER|$NEW_USER|g" "$file"
 done
 
-###############################################################
-# DOWNLOAD SOFTWARES
-###############################################################
-wget -O ./home/Programs/GIMP.AppImage "https://download.gimp.org/gimp/v3.2/linux/GIMP-3.2.4-x86_64.AppImage"
-wget -O ./home/Programs/OpenShot.AppImage "https://github.com/OpenShot/openshot-qt/releases/download/v3.5.1/OpenShot-v3.5.1-x86_64.AppImagee"
-wget -O ./home/Programs/upscayl.AppImage "https://github.com/upscayl/upscayl/releases/download/v2.15.0/upscayl-2.15.0-linux.AppImage"
+cp ./home/* /home/$NEW_USER/
