3064607f9f5b947cd330ad13496f9803e0c55436
AlpasNet <117809035+AlpasNet@users.noreply.github.com>
2026-06-10 15:17:18 +0200

    Implement username change and software downloads
    Added functionality to change the username in files and download software applications.

--- eos-linux-install.sh
+++ eos-linux-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+###############################################################
+# CHANGE USERNAME
+###############################################################
+
 OLD_USER="seijeeos"
 NEW_USER="$1"
 
@@ -14,3 +18,10 @@ echo "Replacing '$OLD_USER' by '$NEW_USER' ..."
 grep -RIl "$OLD_USER" "home" | while read -r file; do
     sed -i "s|$OLD_USER|$NEW_USER|g" "$file"
 done
+
+###############################################################
+# DOWNLOAD SOFTWARES
+###############################################################
+wget -O ./home/Programs/GIMP.AppImage "https://download.gimp.org/gimp/v3.2/linux/GIMP-3.2.4-x86_64.AppImage"
+wget -O ./home/Programs/OnlyOffice.AppImage "https://github.com/ONLYOFFICE/appimage-desktopeditors/releases/download/v9.4.0/DesktopEditors-x86_64.AppImage"
+wget -O ./home/Programs/upscayl.AppImage "https://github.com/upscayl/upscayl/releases/download/v2.15.0/upscayl-2.15.0-linux.AppImage"
